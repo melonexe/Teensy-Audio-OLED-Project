bc6b54b7578514a1ae04ce0ddac1a6e07878a499
Alexander Carswell-Sharp <alexandersimoncs@btinternet.com>
2024-02-20 02:31:51 +0000

    Changes to be committed: 	modified:   src/menu.h
    added a brief explanation for the .h file

--- src/menu.h
+++ src/menu.h
@@ -7,3 +7,7 @@ void displayMenuPage2();
 void displayMenuPage3();
 
 #endif // MENU_H
+
+
+//this is the .h file which contatins information about the functions in the cpp file
+// the main.cpp includes this to be able to use the functions defined in the menus.cpp file
